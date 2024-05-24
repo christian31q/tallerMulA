31bb5a226e22f47ca9a8eca04a81d1fe68595550
jugodelulo03 <est.julian.pinzon@unimilitar.edu.co>
2024-05-24 09:37:03 -0500

    pedir datos de DB

---
+++ conexion.php
@@ -0,0 +1,11 @@
+<?php 
+
+// Datos de conexión a la base de datos
+$servername = "sql207.infinityfree.com";
+$username = "if0_36608319";
+$password = "D0e58nZPbQ";
+$database = "if0_36608319_carnets";
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $database);
+?>
